606e41c9475bc1766aac677c133cf0230643807b
singularity <chency_singularity@163.com>
2023-07-15 21:55:41 +0800

    feat: add react to arch design

--- tests/unit_tests/steps/test_arch_design.py
+++ tests/unit_tests/steps/test_arch_design.py
@@ -3,7 +3,8 @@ from vlsi_flow.steps.arch_design.prompts import DEFAULT_ARCH_DESIGN_PROMPT
 
 
 def test_programming_prompts() -> None:
-    chip_description = """an out of order 32-bit RISC-V CPU that supports standard "G (IMA-FD)" extensions """
+    chip_description = """an out-of-order 32-bit RISC-V CPU that supports standard "G (IMA-FD)" extensions \
+with branch prediction and L1 cache."""
     input_variables = DEFAULT_ARCH_DESIGN_PROMPT.input_variables
     print("input_variables:", input_variables)
     arch_design_prompts = DEFAULT_ARCH_DESIGN_PROMPT.format(
--- vlsi_flow/steps/arch_design/prompts.py
+++ vlsi_flow/steps/arch_design/prompts.py
@@ -3,37 +3,45 @@ from langchain.prompts.pipeline import PipelinePromptTemplate
 from langchain.prompts.prompt import PromptTemplate
 
 _FINAL_ARCH_DESIGN_TEMPLATE = """{introduction_prompt}
-{chip_description_prompt}
-{write_module_definition_prompt}
+{react_prompt}
 {write_module_description_prompt}
+{chip_description_prompt}
 """
 
 introduction_template = """
 You are a senior circuit architect, and you are designing a chip."""
 
-chip_description_template = """
-The chip description is: {chip_description}"""
+react_template = """
+Write your thoughts on designing all modules following the format below:
+- Task: The task you need to complete. If you think the task is too complex to complete in one iteration, refine the task into small ones.
+- Thought: Consider what needs to be done.
+- Action: actions to take.
+- Action Input: Provide the necessary input for the chosen action. You must print all the action inputs. If the action inputs are too long, show part of them.
+- Observation: Record the result of the action.
+... (Thought/Action/Action Input/Observation can repeat N times)
+- Thought: Once you know the final answer.
+- Final Answer: The answer to the original question."""
 
-write_module_definition_template = """
-Please design the modules in this chip in a markdown code block format."""
+chip_description_template = """
+The chip you are designing is: {chip_description}"""
 
 write_module_description_template = """
-Please describe the modules one by one in compact JSON format. \
-For each module, the keys are module name, module function, module description, \
-and module interface.
-The keys of the module interface are the port names, port directions, \
-port widths, and port descriptions."""
+After you get the final answer, describe the modules one by one, in separate compact JSON strings in Markdown blocks. \
+For each module, the keys are module name, module function, module description, and all the module interfaces. \
+All the required module interfaces, \
+such as the clock and reset ports, data ports and control ports, etc, should be designed.
+The keys of the module interface are the port name, port direction, port width, and port description."""
 
 _FINAL_ARCH_DESIGN_PROMPT = PromptTemplate.from_template(_FINAL_ARCH_DESIGN_TEMPLATE)
 introduction_prompt = PromptTemplate.from_template(introduction_template)
+react_prompt = PromptTemplate.from_template(react_template)
 chip_description_prompt = PromptTemplate.from_template(chip_description_template)
-write_module_definition_prompt = PromptTemplate.from_template(write_module_definition_template)
 write_module_description_prompt = PromptTemplate.from_template(write_module_description_template)
 
 arch_design_prompts = [
     ("introduction_prompt", introduction_prompt),
+    ("react_prompt", react_prompt),
     ("chip_description_prompt", chip_description_prompt),
-    ("write_module_definition_prompt", write_module_definition_prompt),
     ("write_module_description_prompt", write_module_description_prompt)
 ]
 DEFAULT_ARCH_DESIGN_PROMPT = PipelinePromptTemplate(
